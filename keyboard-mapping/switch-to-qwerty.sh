43bbcf8d9039f800da82aebd79304022b3370e1a
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-02-16 23:55:11 +0100

    add scripts for switching to rsthd / qwerty

---
+++ keyboard-mapping/switch-to-qwerty.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.config/karabiner-qwerty.edn ~/.config/karabiner.edn
+
+osascript ~/dev/osandell/scripts-osx/keyboard-mapping/switch-to-qwerty-keyboard-layout.applescript
---
+++ keyboard-mapping/switch-to-rsthd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.config/karabiner-rsthd.edn ~/.config/karabiner.edn
+
+osascript ~/dev/osandell/scripts-osx/keyboard-mapping/switch-to-rsthd-keyboard-layout.applescript
