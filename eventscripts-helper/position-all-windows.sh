3e08bc38f9527a0d900e2a731e83fae5e3593d8a
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-08-14 20:35:24 +0200

    use firefox

--- eventscripts-helper/position-all-windows.sh
+++ eventscripts-helper/position-all-windows.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "Google Chrome Music" primary maximized auto
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "Google Chrome Personal" primary maximized auto
+
 osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript Slack primary maximized auto
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "Google Chrome Grebban" primary maximized auto
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "Google Chrome Dev" secondary maximized auto
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript Fork primary custom auto 0.59 1 0.205 0
-osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript kitty primary custom auto 0.47 1 -0.265 0
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "FF Personal" primary maximized auto
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "FF Work" primary maximized auto
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "FF YouTube" primary maximized auto
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript "Google Chrome" secondary maximized auto
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript GitKraken primary custom auto 0.59 1 0.205 0
+osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript kitty primary custom auto 0.41 1 -0.296 0
 osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript Code primary maximized auto
