33719b88a5d4fe0ba02f47843a8004934616462c
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-10-22 03:59:12 +0200

    feat: add light / dark mide switches

---
+++ to-dark-mode.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Store the name of the current active app
+current_app=$(osascript -e 'tell application "System Events" to get the name of the first process whose frontmost is true')
+
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
+
+cp ~/.config/kitty/themes/gruvbox.conf ~/.config/kitty/current-theme.conf
+
+osascript -e 'tell application "kitty" to activate'
+osascript -e 'tell application "System Events" to keystroke "," using {command down, control down}'
+
+sed -i '' '/"workbench.colorTheme":/c\    
+"workbench.colorTheme": "Gruvbox Dark Medium",' ~/.config/Code/User/settings.json
+
+osascript -e 'tell application "GitKraken" to activate'
+osascript -e 'tell application "System Events" to keystroke "p" using {command down}'
+osascript -e 'tell application "System Events" to keystroke "theme"'
+osascript -e 'tell application "System Events" to key code 36'
+osascript -e 'tell application "System Events" to keystroke "gruv"'
+osascript -e 'tell application "System Events" to key code 36'
+osascript -e 'tell application "System Events" to key code 53'
+osascript -e 'tell application "System Events" to key code 53'
+
+osascript -e 'tell application "kitty" to activate'
+
+# Check if the current app is Electron-based, if so, activate VSCode, otherwise activate the originally active app
+if [ "$current_app" == "Electron" ]; then
+    osascript -e 'tell application "Visual Studio Code" to activate'
+else
+    osascript -e "tell application \"$current_app\" to activate"
+fi
