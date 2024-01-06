a3809c3eb9e6da8ada2d4a461ea799072115ed7a
Olof Sandell <83674246+osandell@users.noreply.github.com>
2024-01-06 20:04:44 +0100

    feat: add echo logs

--- soft-reboot.sh
+++ soft-reboot.sh
@@ -6,8 +6,10 @@
 
 # From https://github.com/dflock/kitty-save-session
 # Convert this JSON file into a kitty session file:
+echo "Looking for Kitty socket in /tmp..."
 socket=$(ls /tmp/mykitty-* 2>/dev/null | head -n 1)
 if [[ -n "$socket" ]]; then
+    echo "Found Kitty socket in $socket."
     export KITTY_LISTEN_ON="unix:$socket"
     # Here we could simply do kitty @ ls | python3 ~/.config/kitty/kitty-convert-dump.py >~/.config/kitty/kitty-session.kitty
     # but I use a method that will also work if invoked form outside of kitty for future reference.
@@ -18,13 +20,18 @@ fi
 
 # From https://github.com/dflock/kitty-save-session
 # Convert this JSON file into a kitty session file:
+echo "Dumping Kitty session..."
 kitty @ ls | python3 ~/.config/kitty/kitty-convert-dump.py >~/.config/kitty/kitty-session.kitty
 
+echo "Closing FF Personal..."
 open -a "FF Personal"
 osascript -e 'tell application "FireFox" to quit'
+echo "Closing FF Work..."
 open -a "FF Work"
 osascript -e 'tell application "FireFox" to quit'
+open -a "FF Work"
 open -a "FF YouTube"
 osascript -e 'tell application "FireFox" to quit'
 
+echo "Issuing soft reboot..."
 osascript -e 'tell app "System Events" to restart'
