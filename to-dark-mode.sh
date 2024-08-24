d701da554ad36450660d5cef684467e91e221621
Olof Sandell <83674246+osandell@users.noreply.github.com>
2024-08-24 23:00:11 +0200

    fix: sort out light mode / dark mode switch

--- to-dark-mode.sh
+++ to-dark-mode.sh
@@ -1,38 +1,28 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 
 curl -X POST -d "activateDarkMode" http://localhost:57321
 
-Store the name of the current active app
-current_app=$(cat /tmp/current_app.txt)
+# Store the name of the current active app
+focused_global_app=$(cat /tmp/focused_global_app.txt)
 
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
 
+# Kitty
 cp ~/.config/kitty/themes/gruvbox.conf ~/.config/kitty/current-theme.conf
-
-# # From https://github.com/dflock/kitty-save-session
-# # Convert this JSON file into a kitty session file:
-# socket=$(ls /tmp/mykitty-* 2>/dev/null | head -n 1)
-# if [[ -n "$socket" ]]; then
-#     export KITTY_LISTEN_ON="unix:$socket"
-#     /Applications/kitty.app/Contents/MacOS/kitty @ ls | python3 ~/.config/kitty/kitty-convert-dump.py >~/.config/kitty/kitty-session.kitty
-# else
-#     echo "No Kitty socket found in /tmp."
-# fi
-
-# osascript -e 'tell application "kitty" to quit'
-# osascript -e 'tell application "kitty" to activate'
-
-# osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript kitty primary custom auto 0.406 1 -0.297 0
-
-# osascript -e 'tell application "System Events" to keystroke "," using {command down, control down}'
-
-osascript -e 'tell application "kitty-main" to activate'
+open -a 'Electron'
+open -a 'kitty-main'
+sleep 0.2
+open -a 'kitty-main'
 osascript -e 'tell application "System Events" to keystroke "," using {command down, control down}'
 osascript -e 'tell application "kitty-lf" to activate'
 osascript -e 'tell application "System Events" to keystroke "," using {command down, control down}'
 
+# VSCode
 awk '/"workbench.colorTheme":/ {print "  \"workbench.colorTheme\": \"Gruvbox Dark Medium\","; next}1' /Users/olof/.config/Code/User/settings.json >/tmp/temp.json && mv /tmp/temp.json /Users/olof/.config/Code/User/settings.json
+# For some reason the above command doesn't work for the "olof" workspace, so I need to use a local settings.json for that workspace
+awk '/"workbench.colorTheme":/ {print "  \"workbench.colorTheme\": \"Gruvbox Dark Medium\","; next}1' /Users/olof/.vscode/settings.json >/tmp/temp.json && mv /tmp/temp.json /Users/olof/.vscode/settings.json
 
+# GitKraken
 osascript -e 'tell application "GitKraken" to activate'
 osascript -e 'tell application "System Events" to keystroke "p" using {command down}'
 osascript -e 'tell application "System Events" to keystroke "theme"'
@@ -42,12 +32,19 @@ osascript -e 'tell application "System Events" to key code 36'
 osascript -e 'tell application "System Events" to key code 53'
 osascript -e 'tell application "System Events" to key code 53'
 
-osascript -e 'tell application "kitty-main" to activate'
-
-# Check if the current app is Electron-based, if so, activate VSCode, otherwise activate the originally active app
-if [ "$current_app" == "coding-environment" ]; then
-    open -a "kitty-main"
-    curl -X POST localhost:57321 -d "openCurrentApp"
+# Check if the current app is kitty-main or vscode, if so, activate VSCode, otherwise activate the originally active app
+if [ "$focused_global_app" == "kitty-main" ]; then
+    open -a 'Electron'
+    open -a 'Visual Studio Code'
+    open -a 'kitty-main'
+    sleep 0.2
+    open -a 'kitty-main'
+elif [ "$focused_global_app" == "vscode" ]; then
+    open -a 'Electron'
+    open -a 'kitty-main'
+    open -a 'Visual Studio Code'
+    sleep 0.2
+    open -a 'Visual Studio Code'
 else
-    osascript -e "tell application \"$current_app\" to activate"
+    osascript -e "tell application \"$focused_global_app\" to activate"
 fi
