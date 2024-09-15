e86c70eb534840380bec6a44eaf5ff64c598595f
Olof Sandell <83674246+osandell@users.noreply.github.com>
2024-09-15 17:41:06 +0200

    feat(open-cursor): create file if not existing

--- open-cursor.sh
+++ open-cursor.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 if [[ -z $1 ]]; then
-    # If no argument is provided, open Visual Studio Code in current dir
+    # If no argument is provided, open Cursor in current dir
     open -a Cursor .
 else
-    # If an argument is provided, open Visual Studio Code with the specified path
-    open -a Cursor $1
+    # If an argument is provided, check if it's a file
+    if [[ -f $1 ]]; then
+        # If file exists, open it with Cursor
+        open -a Cursor "$1"
+    else
+        # If file doesn't exist, create it and then open with Cursor
+        touch "$1"
+        open -a Cursor "$1"
+    fi
 fi
-
-# osascript ~/dev/osandell/set-window-boundaries/set-window-boundaries.applescript Code primary maximized auto
